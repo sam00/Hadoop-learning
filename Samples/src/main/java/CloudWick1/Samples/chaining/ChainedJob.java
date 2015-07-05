deff5073bbb6ff5cc4cd705e548bd487c79d273b
sam00 <samanvay89@gmail.com>
2015-07-05 13:45:30 +0100

    changes

--- Samples/src/main/java/CloudWick1/Samples/chaining/ChainedJob.java
+++ Samples/src/main/java/CloudWick1/Samples/chaining/ChainedJob.java
@@ -16,12 +16,18 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 /*
  * This exercise is about foo and bar. It works like this
- * - Map a to b
- * - Map b to c
- * - Reduce c
+ * - Map Mapper1 to Mapper2
+ * - 
+ * 
  * 
  * Input is this
+ * Unique_artist
+ * Unique_tracks
+ * 
  * Output is that
+ * 
+ * String related to night
+ * 
  * */
 public class ChainedJob {
 
@@ -36,7 +42,7 @@ public class ChainedJob {
     Configuration conf = new Configuration(true);
 
     // Create job
-    Job job = new Job(conf, "WordCount");
+    Job job = new Job(conf, "ChainedJob");
     job.setJarByClass(ChainedJob.class);
 
     // config1
@@ -102,68 +108,3 @@ public class ChainedJob {
 
 
 
-//
-//import java.io.IOException;
-//
-//import org.apache.hadoop.conf.Configuration;
-//import org.apache.hadoop.fs.FileSystem;
-//import org.apache.hadoop.fs.Path;
-//import org.apache.hadoop.io.Text;
-// import org.apache.hadoop.mapred.lib.MultipleInputs;
-//import org.apache.hadoop.mapreduce.Job;
-//import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-//import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-//import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-//import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-// 
-//public class WordCount {
-// 
-//    public static void main(String[] args) throws IOException,
-//            InterruptedException, ClassNotFoundException {
-// 
-// Path inputPath1 = new Path(args[0]);
-// Path inputPath2 = new Path(args[1]);
-// Path outputDir = new Path(args[2]);
-// 
-//        // Create configuration
-//        Configuration conf = new Configuration(true);
-// 
-//        // Create job
-//        Job job = new Job(conf, "WordCount");
-//        job.setJarByClass(WordCountMapper.class);
-// 
-//        // Setup MapReduce
-//        job.setMapperClass(WordCountMapper.class);
-// job.setReducerClass(WordCountReducer.class);
-//        job.setNumReduceTasks(1);
-// 
-//        // Specify key / value
-//        job.setOutputKeyClass(Text.class);
-//    job.setOutputValueClass(Text.class);
-// 
-//        // Input
-//
-// MultipleInputs.addInputPath(job, inputPath1, TextInputFormat.class,
-// WordCountMapper.class);
-// MultipleInputs.addInputPath(job, inputPath2, TextInputFormat.class,
-// Map1.class);
-//
-// // FileInputFormat.addInputPath(job, inputPath);
-// // job.setInputFormatClass(TextInputFormat.class);
-// 
-//        // Output
-//        FileOutputFormat.setOutputPath(job, outputDir);
-//        job.setOutputFormatClass(TextOutputFormat.class);
-// 
-//        // Delete output if exists
-//        FileSystem hdfs = FileSystem.get(conf);
-//        if (hdfs.exists(outputDir))
-//            hdfs.delete(outputDir, true);
-// 
-//        // Execute job
-//        int code = job.waitForCompletion(true) ? 0 : 1;
-//        System.exit(code);
-// 
-//    }
-// 
-//}
