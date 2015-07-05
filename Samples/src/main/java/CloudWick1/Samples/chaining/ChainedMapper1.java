f2f0d96014f89519e8b7b492595280f44da38426
sam00 <samanvay89@gmail.com>
2015-07-05 13:39:30 +0100

    changes

--- Samples/src/main/java/CloudWick1/Samples/chaining/ChainedMapper1.java
+++ Samples/src/main/java/CloudWick1/Samples/chaining/ChainedMapper1.java
@@ -38,34 +38,3 @@ public class ChainedMapper1 extends Mapper<Object, Text, Text, Text> {
     }
   }
 }
-
-
-
-// import java.io.IOException;
-//
-// import org.apache.hadoop.io.Text;
-// import org.apache.hadoop.mapreduce.Mapper;
-//
-//
-//
-// public class WordCountMapper extends Mapper<Object, Text, Text, Text> {
-//
-// public void map(Object key, Text value, Context context) throws IOException,
-// InterruptedException {
-//
-// String[] splitted = value.toString().split("<SEP>");
-// String searchWord = "night";
-// int titleIndex = 3;
-//
-// if (splitted.length == titleIndex + 1) {
-//
-// boolean containsSearchWord =
-// splitted[titleIndex].toLowerCase().contains(searchWord);
-// if (containsSearchWord) {
-//
-// context.write(new Text(), new Text(value));
-// }
-//
-// }
-// }
-// }
