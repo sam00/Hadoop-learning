7ad657ca799c145d2bb37ae6092641f3c4334c7d
sam00 <samanvay89@gmail.com>
2015-07-07 16:30:05 +0100

    changes

--- Samples/src/main/java/cloudwick/samples/uniquecount/UniqueCountReducer.java
+++ Samples/src/main/java/cloudwick/samples/uniquecount/UniqueCountReducer.java
@@ -6,8 +6,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class UniqueCountReducer extends
- Reducer<Text, IntWritable, Text, Text> {
+public class UniqueCountReducer extends Reducer<Text, IntWritable, Text, Text> {
 
   public void reduce(Text text, Iterable<IntWritable> values, Context context)
       throws IOException, InterruptedException {
