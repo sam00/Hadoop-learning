ca66c6f5969825e91154969eeddb341f4ba7f391
sam00 <samanvay89@gmail.com>
2015-07-15 10:19:26 +0100

    Zip file

---
+++ Samples/src/main/java/AnnualIncome/ZipFormat.java
@@ -0,0 +1,43 @@
+package AnnualIncome;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+public class ZipFormat extends FileInputFormat<Text, BytesWritable> {
+  // See the comments on the setLenient() method
+  private static boolean isLenient = false;
+
+  // ZIP files are not splitable
+
+  @Override
+  protected boolean isSplitable(JobContext context, Path filename) {
+    return false;
+  }
+
+  // Create the ZipFileRecordReader to parse the file
+
+  @Override
+  public RecordReader<Text, BytesWritable> createRecordReader(InputSplit split,
+      TaskAttemptContext context) throws IOException, InterruptedException {
+    return new ZipRecord();
+  }
+
+  // @param lenient
+
+  public static void setLenient(boolean lenient) {
+    isLenient = lenient;
+  }
+
+  public static boolean getLenient() {
+    return isLenient;
+  }
+}
+
